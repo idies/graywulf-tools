05ad0b5aff646b397864c9bc56c2b5488749a55c
Laszlo Dobos <dobos@complex.elte.hu>
2015-02-26 09:55:27 -0500

    Clr functions added.

---
+++ dll/Jhu.Graywulf.DBSubset.SqlClrLib/DBSubset/SqlClrLib/Random.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Collections.Generic;
+using Microsoft.SqlServer.Server;
+using System.Threading;
+
+public partial class UserDefinedFunctions
+{
+    private static readonly Random seeder = new Random();
+
+    [ThreadStatic]
+    private static Random random;
+
+    [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.None, IsDeterministic = false, IsPrecise = false)]
+    public static SqlDouble RandomDouble()
+    {
+        if (random == null)
+        {
+            lock (seeder)
+            {
+                random = new Random(seeder.Next());
+            }
+        }
+
+        return new SqlDouble(random.NextDouble());
+    }
+
+    [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.None, IsDeterministic = false, IsPrecise = true)]
+    public static SqlInt32 RandomInt(SqlInt32 max)
+    {
+        if (random == null)
+        {
+            lock (seeder)
+            {
+                random = new Random(seeder.Next());
+            }
+        }
+
+        return new SqlInt32(random.Next(max.Value));
+    }
+};
+
---
+++ dll/Jhu.Graywulf.DBSubset.SqlClrLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Jhu.Graywulf.DBSubset.SqlClrLib")]
+[assembly: AssemblyDescription("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("7ff03847-c37b-4abf-898a-a7d18c0bda62")]
