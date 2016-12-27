eed6c4faef5138badb605aef07cacc39c70bfa94
Laszlo Dobos <dobos@complex.elte.hu>
2016-12-27 17:02:27 -0500

    Copy install scripts to target dir.

--- dll/Jhu.Graywulf.DBSubset.SqlClrLib/post-build.ps1
+++ dll/Jhu.Graywulf.DBSubset.SqlClrLib/post-build.ps1
@@ -11,4 +11,5 @@
 
 cp $ProjectDir$OutDir$TargetName.dll $SolutionDir$OutDir
 cp $ProjectDir$OutDir$TargetName.pdb $SolutionDir$OutDir
-
+cp $ProjectDir$OutDir$TargetName.Create.sql $SolutionDir$OutDir
+cp $ProjectDir$OutDir$TargetName.Drop.sql $SolutionDir$OutDir
